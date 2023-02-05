792f872de2e333f56994443aa51c31e6b90cd0ab
Stepan Nemchenko <st-nemchenko@yandex.ru>
2023-02-05 14:22:13 +0300

    Предварительное отображение букв

--- app/src/main/java/com/oneseed/hangman/GameFragment.kt
+++ app/src/main/java/com/oneseed/hangman/GameFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.oneseed.hangman.databinding.FragmentGameBinding
 import com.oneseed.hangman.databinding.FragmentLaunchBinding
 
 
 class GameFragment : Fragment() {
+    private val rcAdapter = LettersAdapter()
     private lateinit var binding: FragmentGameBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -20,6 +23,21 @@ class GameFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val imageRc: RecyclerView = view.findViewById(R.id.abcButtonsRecycler)
+        imageRc.adapter = rcAdapter
+        rcAdapter.lettersList = ArrayList()
+        for (i in 'А'..'Я'){
+            rcAdapter.addLetter(
+                LettersItem(i.toString())
+            )
+        }
+
+
+        rcAdapter.notifyItemChanged(rcAdapter.itemCount)
+
+        imageRc.layoutManager = GridLayoutManager(context, 5)
+
         super.onViewCreated(view, savedInstanceState)
     }
 
