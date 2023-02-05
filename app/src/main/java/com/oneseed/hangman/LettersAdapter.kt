29ba95b91d333382779dc87f4ba3dd99681657c6
Stepan Nemchenko <st-nemchenko@yandex.ru>
2023-02-05 13:59:31 +0300

    Добавлен файл адаптера LettersAdapter

---
+++ app/src/main/java/com/oneseed/hangman/LettersAdapter.kt
@@ -0,0 +1,44 @@
+package com.oneseed.hangman
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.oneseed.hangman.databinding.RussianAbcButtonBinding
+
+class LettersAdapter : RecyclerView.Adapter<LettersAdapter.LettersHolder>() {
+    var lettersList = ArrayList<LettersItem>()
+
+    class LettersHolder(item: View) : RecyclerView.ViewHolder(item) {
+        private val binding = RussianAbcButtonBinding.bind(item)
+
+        fun bind(letterItem: LettersItem) {
+            binding.tvTitle.text = letterItem.letter
+
+        }
+
+    }
+
+    fun addLetter(letterItem: LettersItem) {
+        lettersList.add(letterItem)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LettersHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.russian_abc_button, parent, false)
+
+        return LettersHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: LettersHolder, position: Int) {
+        holder.bind(lettersList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return lettersList.size
+    }
+}
+
+data class LettersItem(
+    val letter: String,
+)
