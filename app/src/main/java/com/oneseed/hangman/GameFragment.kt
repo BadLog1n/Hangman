9825ff17fa58d0dbc54557e28407f96c989ccc79
Stepan Nemchenko <st-nemchenko@yandex.ru>
2023-02-07 12:58:08 +0300

    Добавлен таймер

--- app/src/main/java/com/oneseed/hangman/GameFragment.kt
+++ app/src/main/java/com/oneseed/hangman/GameFragment.kt
@@ -1,15 +1,21 @@
 package com.oneseed.hangman
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.oneseed.hangman.databinding.FragmentGameBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.broadcast
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class GameFragment : Fragment(), LettersAdapter.RecyclerViewEvent {
@@ -17,6 +23,7 @@ class GameFragment : Fragment(), LettersAdapter.RecyclerViewEvent {
     private val rcAdapter = LettersAdapter(lettersList, this)
     private val inputString = "тест".uppercase()
     private var trying = 0
+    private var timerCount = 30
     private lateinit var arrayOfAnswers: CharArray
     private lateinit var binding: FragmentGameBinding
     override fun onCreateView(
@@ -36,13 +43,36 @@ class GameFragment : Fragment(), LettersAdapter.RecyclerViewEvent {
                 LettersItem(i.toString())
             )
         }
+        rcAdapter.notifyItemChanged(rcAdapter.itemCount)
+        imageRc.layoutManager = GridLayoutManager(context, 5)
 
         val localArray = CharArray(inputString.length) { ' ' }
         arrayOfAnswers = localArray
 
-        rcAdapter.notifyItemChanged(rcAdapter.itemCount)
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                while (timerCount > 0) {
+                    binding.timer.text = timerCount.toString()
+                    Thread.sleep(1000)
+                    timerCount--
+                    if (timerCount == 0) {
+                        withContext(Dispatchers.Main) {
+                            AlertDialog.Builder(context)
+                                .setTitle("Вы проиграли!")
+                                .setMessage("Время вышло!")
+                                .setCancelable(false)
+                                .setPositiveButton("OK") { _, _ ->
+                                    findNavController().navigateUp()
+                                }
+                                .show()
+                        }
+                        timerCount = -1
+                    }
+                }
+            }
+        }
 
-        imageRc.layoutManager = GridLayoutManager(context, 5)
+
 
         super.onViewCreated(view, savedInstanceState)
     }
@@ -50,41 +80,54 @@ class GameFragment : Fragment(), LettersAdapter.RecyclerViewEvent {
 
     override fun onItemClicked(letter: Char) {
         if (inputString.contains(letter)) {
-            for (i in inputString.indices) {
-                if (inputString[i] == letter) {
-                    arrayOfAnswers[i] = letter
-                }
-            }
-            binding.inputCode.code = String(arrayOfAnswers)
-            if (String(arrayOfAnswers) == inputString) {
-                AlertDialog.Builder(context)
-                    .setTitle("Поздравляем!")
-                    .setMessage("Вы угадали слово!")
-                    .setCancelable(false)
-                    .setPositiveButton("OK") { _, _ ->
-                        findNavController().navigateUp()
-                    }
-                    .show()
+            isRightAnswer(letter)
+        } else {
+            isWrongAnswer()
+        }
 
+    }
+
+
+    private fun isRightAnswer(letter: Char) {
+        for (i in inputString.indices) {
+            if (inputString[i] == letter) {
+                arrayOfAnswers[i] = letter
             }
         }
-        else {
-            trying++
-            if (trying == 6) {
-                AlertDialog.Builder(context)
-                    .setTitle("Вы проиграли!")
-                    .setMessage("Вы не угадали слово!")
-                    .setCancelable(false)
-                    .setPositiveButton("OK") { _, _ ->
-                        findNavController().navigateUp()
-                    }
-                    .show()
-            }
+        binding.inputCode.code = String(arrayOfAnswers)
+        if (String(arrayOfAnswers) == inputString) {
+            AlertDialog.Builder(context)
+                .setTitle("Поздравляем!")
+                .setMessage("Вы угадали слово!")
+                .setCancelable(false)
+                .setPositiveButton("OK") { _, _ ->
+                    findNavController().navigateUp()
+                }
+                .show()
+            timerCount = -1
+
+        }
+    }
+
+    @SuppressLint("DiscouragedApi")
+    private fun isWrongAnswer() {
+        trying++
+        if (trying == 6) {
+            AlertDialog.Builder(context)
+                .setTitle("Вы проиграли!")
+                .setMessage("Вы не угадали слово!")
+                .setCancelable(false)
+                .setPositiveButton("OK") { _, _ ->
+                    findNavController().navigateUp()
+                }
+                .show()
+            timerCount = -1
+
+        } else {
             val image = "@drawable/hangman_$trying"
             val resID = resources.getIdentifier(image, "drawable", activity?.packageName)
             binding.imageView.setImageResource(resID)
         }
-
     }
 
 
